0cd5720897ced7686d1fc2f5bdeda0c8b7581fc7
Louis Young <me@louisyoung.co.uk>
2021-01-27 17:25:37 +0000

    Build update retrieval on client portal

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ const formatRegistration = (registration) => {
 const App = () => {
   const [vehicle, setVehicle] = useState({});
 
+  const [updates, setUpdates] = useState([]);
+
   const search = async (registration) => {
     try {
       const formattedRegistration = formatRegistration(registration);
@@ -21,17 +23,43 @@ const App = () => {
       const vehicle = await response.json();
 
       setVehicle(vehicle);
+
+      getUpdates(vehicle._id);
     } catch (error) {
       console.error(error);
     }
   };
+
+  const getUpdates = async (vehicle) => {
+    try {
+      const url = `http://localhost:5000/updates/${vehicle}`;
+
+      const response = await fetch(url);
+
+      const updates = await response.json();
+
+      setUpdates(updates.updates);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(updates);
 
   return (
     <>
       <h1>Garageline</h1>
       <Search search={search} />
 
+      <p>{vehicle.registration}</p>
       <p>{vehicle.colour}</p>
+
+      {updates.map(({ status, technician }) => (
+        <li>
+          <p>Status: {status}</p>
+          {technician && <p>Technician: {technician}</p>}
+        </li>
+      ))}
     </>
   );
 };
